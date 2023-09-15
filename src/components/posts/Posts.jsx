8cf325683af27e8807f7da1f1edae2cd4a7f77fe
Hemaet33 <hemaet3333@gmail.com>
2023-09-16 03:40:15 +0600

    changes

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -6,22 +6,20 @@ import axios from 'axios';
 
 function Posts({userId}) {
     const [posts, setPosts] = useState([]);
-    const [havePosts, setHavePosts] = useState(null);
+
     useEffect(()=>{
       const userPosts = async()=>{
         try {
           if(userId){
             const res = await axios.get("https://react-social-api.onrender.com/api/posts?userId="+userId.id,{
-            withCredentials: true
+            withCredentials: !havePosts
           });
-          await setPosts(res.data);
-          res.data.length>0 ? setHavePosts(true) : setHavePosts(false)
+          setPosts(res.data);
           }else{
             const res = await axios.get("https://react-social-api.onrender.com/api/posts/",{
             withCredentials: true
           });
-          await setPosts(res.data);
-          res.data.length>0 ? setHavePosts(true) : setHavePosts(false)
+          setPosts(res.data);
           }
         } catch (error) {
           console.log(error);
@@ -31,7 +29,7 @@ function Posts({userId}) {
     },[]);
   return (
     <div className='posts'>
-      {havePosts ? <h1>No posts to show.</h1> :
+      {posts.length>0 ? <h1>No posts to show.</h1> :
         posts.map(post=>(
           <Post post={post} key={post.id} userId = {userId} />
         ))
