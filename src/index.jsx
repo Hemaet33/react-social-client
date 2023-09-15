9b75716abb419d23dfcea6dea55c2ae2f3a0e3b2
Hemaet33 <hemaet3333@gmail.com>
2023-09-16 00:53:08 +0600

    react-social

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,8 @@ import App from './App.jsx'
 import {DarkModeContextProvider } from './contexts/darkModeContext.jsx'
 import { AuthContextProvider } from './contexts/authContext.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
   <DarkModeContextProvider>
   <AuthContextProvider>
